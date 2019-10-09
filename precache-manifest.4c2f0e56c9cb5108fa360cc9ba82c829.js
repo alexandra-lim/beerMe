bf4a76949c71e1a47c4fd7e00646289f66931956
Alexandra Lim <alexandra.lim@gmail.com>
2019-10-09 12:37:11 -0400

    Updates

---
+++ precache-manifest.4c2f0e56c9cb5108fa360cc9ba82c829.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd6a97bc79c095ae08e21fc7d49f8bc6",
+    "url": "/beerMe/index.html"
+  },
+  {
+    "revision": "6182458cd3fb21c02351",
+    "url": "/beerMe/static/css/main.0fb88b9d.chunk.css"
+  },
+  {
+    "revision": "beaf8a59162a54d171ca",
+    "url": "/beerMe/static/js/2.6db90cc4.chunk.js"
+  },
+  {
+    "revision": "6182458cd3fb21c02351",
+    "url": "/beerMe/static/js/main.9e4ee27a.chunk.js"
+  },
+  {
+    "revision": "7209a3208b135336c0df",
+    "url": "/beerMe/static/js/runtime-main.5b52ed5c.js"
+  }
+]);
